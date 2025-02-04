05cde68e309cc64032a3c38cf3ea48e93ea657a8
Diederichet <ethan@thedfamily.com>
2025-02-04 00:12:47 +0000

    test file added

---
+++ bot_test.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='>', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+	await ctx.send('pong')
+
+with open('creds.txt') as file:
+    firstline = file.readline()
+
+bot.run(firstline)
+
