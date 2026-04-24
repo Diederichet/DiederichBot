672a2c19f35767d00268898d1840163b4126bcea
Ethan Diederich <90162918+Diederichet@users.noreply.github.com>
2026-04-24 19:25:27 -0400

    Refactor commands and add quote system
    Refactor eightball responses and improve age command formatting. Added quote system with quiz functionality.

--- functions/fun.py
+++ functions/fun.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import random
 import aiohttp
+import asyncio
 
 
 class Fun(commands.Cog):
@@ -31,25 +32,12 @@ class Fun(commands.Cog):
     @commands.command()
     async def eightball(self, ctx, *, question: str):
         responses = [
-            "It is certain",
-            "Reply hazy, try again",
-            "Don’t count on it",
-            "It is decidedly so",
-            "Ask again later",
-            "My reply is no",
-            "Without a doubt",
-            "Better not tell you now",
-            "My sources say no",
-            "Yes definitely",
-            "Cannot predict now",
-            "Outlook not so good",
-            "You may rely on it",
-            "Concentrate and ask again",
-            "Very doubtful",
-            "As I see it, yes",
-            "Most likely",
-            "Outlook good",
-            "Yes",
+            "It is certain", "Reply hazy, try again", "Don’t count on it",
+            "It is decidedly so", "Ask again later", "My reply is no",
+            "Without a doubt", "Better not tell you now", "My sources say no",
+            "Yes definitely", "Cannot predict now", "Outlook not so good",
+            "You may rely on it", "Concentrate and ask again", "Very doubtful",
+            "As I see it, yes", "Most likely", "Outlook good", "Yes",
             "Signs point to yes"
         ]
 
@@ -66,35 +54,97 @@ class Fun(commands.Cog):
 
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as resp:
-
                 if resp.status != 200:
                     await ctx.send("Could not fetch a fact.")
                     return
 
                 data = await resp.json()
-
                 fact = data.get("message", {}).get("fact")
 
-                if fact:
-                    await ctx.send(fact)
-                else:
-                    await ctx.send("Fact format error.")
+                await ctx.send(fact or "Fact format error.")
+
 
     @commands.command()
     async def age(self, ctx, *, name: str):
-        url = "https://api.agify.io?name=" + name
+        url = f"https://api.agify.io?name={name}"
+
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as resp:
-
                 if resp.status != 200:
                     await ctx.send("Response error.")
                     return
+
                 data = await resp.json()
                 guess = data.get("age")
+
                 if guess:
-                    await ctx.send("The estimated age of the name " + name + " is: " + guess)
+                    await ctx.send(
+                        f"The estimated age of {name} is: {guess}"
+                    )
                 else:
                     await ctx.send("Age response format error.")
+
+
+    # -------------------------
+    # Quote system
+    # -------------------------
+
+    async def get_quote(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "https://api.breakingbadquotes.xyz/v1/quotes"
+            ) as resp:
+                data = await resp.json()
+
+        return data[0]["quote"], data[0]["author"]
+
+
+    async def run_quote_quiz(self, ctx, quote, author):
+
+        await ctx.send(
+            f"**Who said this?**\n\n"
+            f"\"{quote}\"\n\n"
+            f"You have 2 minutes."
+        )
+
+        def check(message):
+            return (
+                message.author == ctx.author
+                and message.channel == ctx.channel
+            )
+
+        try:
+            msg = await self.bot.wait_for(
+                "message",
+                timeout=120.0,
+                check=check
+            )
+
+            guess = msg.content.lower()
+            correct = author.lower()
+
+            if any(part in guess for part in correct.split()):
+                await ctx.send(f"Correct! It was **{author}**.")
+            else:
+                await ctx.send(f"WRONG. It was **{author}**.")
+
+        except asyncio.TimeoutError:
+            await ctx.send(f"Time's up! It was **{author}**.")
+
+
+    @commands.command()
+    async def bbquote(self, ctx, mode: str = None):
+
+        quote, author = await self.get_quote()
+
+        if mode == "quiz":
+            await self.run_quote_quiz(ctx, quote, author)
+            return
+
+        await ctx.send(
+            f"\"{quote}\"\n"
+            f"- **{author}**"
+        )
 
 
 async def setup(bot):
